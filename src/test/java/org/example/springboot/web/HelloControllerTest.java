7506a040957f23d3aa4ed1bb4948d9b1e691f773
HyominAn0401 <slhm7547@naver.com>
2024-02-09 00:01:15 +0900

    Controller with Test Code

---
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -0,0 +1,60 @@
+package org.example.springboot.web;
+
+// JUnit 5
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest{
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
+
+// JUnit 4
+//import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servelt.MockMvc;
+//import org.springframework.test.web.servelt.ResultActions;
+//import static org.springframework.test.web.servelt.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = HelloController.class)
+//public class HelloControllerTest {
+//
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @Test
+//    public void hello_return() throws Exception{
+//        String hello = "hello";
+//
+//        mvc.perform(get("/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(hello));
+//    }
+//}

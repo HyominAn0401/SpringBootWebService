6e7415a391cb1327517e04d176891a24ddc81fae
HyominAn0401 <slhm7547@naver.com>
2024-02-10 20:40:05 +0900

    Posts API

--- src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
@@ -40,5 +41,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2024,2,10,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+
     }
 }
